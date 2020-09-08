adabf0c2e4cf7b8e1803ae580def221f09a6adb4
Jean-Francois Demers <5713473+jfdemers@users.noreply.github.com>
2020-09-08 19:38:46 -0400

    Initial Commit

---
+++ RemoteControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RemoteControl
+//
+//  Created by Jean-Francois Demers on 2020-09-08.
+//  Copyright © 2020 Jean-Francois Demers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
